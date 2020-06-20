16c795a5d6c5f7d9338b593a126730deb689bfef
Gonzalo Tanoira <gonzalo.mtanoira@gmail.com>
2020-06-20 18:17:56 -0300

    Quité la api /user_collection/actualizar

--- src/services/user_collection.service.ts
+++ src/services/user_collection.service.ts
@@ -1,16 +1,9 @@
 import { getConnection } from 'typeorm';
 
 // Models
-import { PaymentCommit } from '../models/payment_commit.model';
 import { UserCollection } from '../models/user_collection.model';
 
 export class UserCollectionService {
-
-  // Grabar un nuevo registro en la tabla
-  public async create(userCollection: UserCollection): Promise<UserCollection> {
-    const connection = getConnection('DWHBP');
-    return await connection.getRepository(UserCollection).save(userCollection);
-  }
 
   // Listar todos los registros de la tabla
   public async list(): Promise<UserCollection[]> {  
